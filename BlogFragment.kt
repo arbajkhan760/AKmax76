aae2ae7f751eb571eb4fccfcac4e42b27a53ffd8
arbajkhan760 <arbajkhan760000@gmail.com>
2025-05-25 12:31:36 +0530

    Update BlogFragment.kt

--- BlogFragment.kt
+++ BlogFragment.kt
@@ -1,1 +1,18 @@
-// Placeholder for BlogFragment.kt
+// BlogFragment.kt
+package com.akgroup76.akmax.ui.blog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.akgroup76.akmax.R
+
+class BlogFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_blog, container, false)
+    }
+}// Placeholder for BlogFragment.kt
