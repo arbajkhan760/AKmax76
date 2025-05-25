02a449d15aac47f833e81eb98ee1057f23e59fb8
arbajkhan760 <arbajkhan760000@gmail.com>
2025-05-25 12:27:16 +0530

    Update Article.kt

--- Article.kt
+++ Article.kt
@@ -1,1 +1,10 @@
-// Placeholder for Article.kt
+// Placeholder for Article.kt// Book.kt
+package com.akgroup76.akmax.model
+
+data class Book(
+    val id: String = "",
+    val title: String = "",
+    val description: String = "",
+    val author: String = "",
+    val coverUrl: String = ""
+)
