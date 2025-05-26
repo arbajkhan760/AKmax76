2ce9d51356e172d6ba767198ab7531051edecbc8
arbajkhan760 <arbajkhan760000@gmail.com>
2025-05-26 09:40:43 +0530

    Update Article.kt

--- Article.kt
+++ Article.kt
@@ -1,10 +1,12 @@
-// Article.kt
 package com.akgroup76.akmax.model
 
 data class Article(
     val id: String = "",
     val title: String = "",
     val content: String = "",
-    val imageUrl: String = "",
-    val author: String = ""
+    val author: String = "",
+    val coverImageUrl: String = "",
+    val price: Double = 0.0,
+    val uploaderId: String = "",
+    val timestamp: Long = System.currentTimeMillis()
 )
