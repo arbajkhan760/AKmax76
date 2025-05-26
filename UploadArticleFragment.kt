03041c604d0e3a49ed12d4a9e49491caa38842c6
arbajkhan760 <arbajkhan760000@gmail.com>
2025-05-26 10:00:21 +0530

    Update UploadArticleFragment.kt

--- UploadArticleFragment.kt
+++ UploadArticleFragment.kt
@@ -1,1 +1,39 @@
-// Placeholder for UploadArticleFragment.kt
+package com.akgroup76.akmax.ui.blog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.akgroup76.akmax.databinding.FragmentUploadArticleBinding
+import com.akgroup76.akmax.model.Article
+
+class UploadArticleFragment : Fragment() {
+
+    private lateinit var binding: FragmentUploadArticleBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentUploadArticleBinding.inflate(inflater, container, false)
+
+        binding.btnUpload.setOnClickListener {
+            val title = binding.etTitle.text.toString()
+            val content = binding.etContent.text.toString()
+            val author = binding.etAuthor.text.toString()
+            val price = binding.etPrice.text.toString().toDoubleOrNull() ?: 0.0
+
+            val article = Article(
+                title = title,
+                content = content,
+                author = author,
+                price = price
+            )
+
+            // Upload article to Firebase
+        }
+
+        return binding.root
+    }
+}
