f1daced38a5c689859031486b247aaab1d344a5f
arbajkhan760 <arbajkhan760000@gmail.com>
2025-05-26 09:41:59 +0530

    Update BlogFragment.kt

--- BlogFragment.kt
+++ BlogFragment.kt
@@ -1,4 +1,3 @@
-// BlogFragment.kt
 package com.akgroup76.akmax.ui.blog
 
 import android.os.Bundle
@@ -6,13 +5,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.akgroup76.akmax.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.akgroup76.akmax.databinding.FragmentBlogBinding
+import com.akgroup76.akmax.model.Book
+import com.akgroup76.akmax.model.Article
 
 class BlogFragment : Fragment() {
+
+    private lateinit var binding: FragmentBlogBinding
+    private val bookList = listOf<Book>() // Replace with Firebase or mock data
+    private val articleList = listOf<Article>() // Replace with Firebase or mock data
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_blog, container, false)
+    ): View {
+        binding = FragmentBlogBinding.inflate(inflater, container, false)
+
+        binding.recyclerBooks.layoutManager = LinearLayoutManager(context)
+        binding.recyclerArticles.layoutManager = LinearLayoutManager(context)
+
+        binding.recyclerBooks.adapter = BookAdapter(bookList)
+        binding.recyclerArticles.adapter = ArticleAdapter(articleList)
+
+        return binding.root
     }
-}// Placeholder for BlogFragment.kt
+}
