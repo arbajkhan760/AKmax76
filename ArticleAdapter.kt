7e7b2cbc683ce1889496d2466143ea76945a0848
arbajkhan760 <arbajkhan760000@gmail.com>
2025-05-26 09:56:18 +0530

    Update ArticleAdapter.kt

--- ArticleAdapter.kt
+++ ArticleAdapter.kt
@@ -1,1 +1,28 @@
-// Placeholder for ArticleAdapter.kt
+package com.akgroup76.akmax.ui.blog
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.akgroup76.akmax.databinding.ItemArticleBinding
+import com.akgroup76.akmax.model.Article
+
+class ArticleAdapter(private val articles: List<Article>) :
+    RecyclerView.Adapter<ArticleAdapter.ArticleViewHolder>() {
+
+    inner class ArticleViewHolder(val binding: ItemArticleBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ArticleViewHolder {
+        val binding = ItemArticleBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ArticleViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ArticleViewHolder, position: Int) {
+        val article = articles[position]
+        holder.binding.tvTitle.text = article.title
+        holder.binding.tvAuthor.text = article.author
+        // Load image and price using Glide or Coil
+    }
+
+    override fun getItemCount() = articles.size
+    }
